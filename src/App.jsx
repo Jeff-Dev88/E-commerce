23a4e7302e518a6e7d79995b402b7defe44e207b
Jeff-Dev88 <jeffoliveira71@gmail.com>
2022-11-12 18:42:47 -0300

    Initial

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import reactLogo from "./assets/react.svg";
+import GlobalStyle from "./styles/global-styles";
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <GlobalStyle />
+        <h1>Hello World!</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
