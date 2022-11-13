2e06daba77f3cd94851418e562b1e541b3007192
Jeff-Dev88 <jeffoliveira71@gmail.com>
2022-11-12 21:17:13 -0300

    add chrakra ui
    Add Chakra Ui styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import reactLogo from "./assets/react.svg";
-import GlobalStyle from "./styles/global-styles";
 
 function App() {
   return (
     <div className="App">
       <div>
-        <GlobalStyle />
         <h1>Hello World!</h1>
       </div>
     </div>
