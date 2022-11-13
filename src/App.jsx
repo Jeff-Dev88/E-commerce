ed3ff6498067d846d4e28f4b6a2203486e1b6539
Jeff-Dev88 <jeffoliveira71@gmail.com>
2022-11-12 21:55:40 -0300

    Add navbar
    Add navbar with Chakra UI template

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import reactLogo from "./assets/react.svg";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
       <div>
+        <Navbar />
         <h1>Hello World!</h1>
       </div>
     </div>
